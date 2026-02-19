326f2b2347efa1eb03eacd0bc656511ae241e2f9
ElVirtualJefe <phelps.jeffp@gmail.com>
2026-02-18 22:00:31 -0600

    Moved protobufs and updated  config

--- main/app/helpers/config.py
+++ main/app/helpers/config.py
@@ -7,13 +7,14 @@ else:
     from helpers.constants import APPLICATION_NAME,DEFAULT_DEBUG_LOG_FILE
     from helpers.common import whoami
 
+current_frame_name = whoami(logging.currentframe())
 #from helpers.logger import AppLogger
-from inspect import currentframe
+#from inspect import currentframe
 
-mod_logger = logging.getLogger(f'{APPLICATION_NAME}').getChild(f'{whoami(currentframe())}')
+mod_logger = logging.getLogger(f'{APPLICATION_NAME}').getChild(f'{current_frame_name}')
 #mod_logger.propagate = True
 #mod_logger.debug('Entering module %s', whoami(currentframe()))
-logging.debug('Entering module %s', whoami(currentframe()))
+mod_logger.debug('Entering module %s', current_frame_name)
 #print(f'{mod_logger.name=}')
 #print(f'{mod_logger.parent=}')
 #print(f'{mod_logger.parent=}')
@@ -25,8 +26,8 @@ def check_for_config_file(config_file="config.ini") -> str:
     
     :param confFile: Location for configuration file.
     """
-
-    logging.debug('Entering function %s', whoami(currentframe()))
+    current_func_name = whoami(logging.currentframe())
+    logging.debug('Entering function %s', current_func_name)
 
     try:
         from pathlib import Path
@@ -40,7 +41,7 @@ def check_for_config_file(config_file="config.ini") -> str:
             return 'DEFAULTS'
 
     finally:
-        logging.debug('Leaving function %s', whoami(currentframe()))
+        logging.debug('Leaving function %s', current_func_name)
 
 
 def process_config(config_file):
@@ -49,7 +50,8 @@ def process_config(config_file):
     
     :param config_file: str -> File location of config file
     """
-    logging.debug('Entering function %s', whoami(currentframe()))
+    current_func_name = whoami(logging.currentframe())
+    logging.debug('Entering function %s', current_func_name)
 
     try:
         from configparser import ConfigParser
@@ -59,7 +61,7 @@ def process_config(config_file):
 
         return config
     finally:
-        logging.debug('Leaving function %s', whoami(currentframe()))
+        logging.debug('Leaving function %s', current_func_name)
 
 conf_file = check_for_config_file()
 conf = process_config(conf_file)
@@ -69,25 +71,27 @@ conf = process_config(conf_file)
 Constants Section for Config Values
 """
 
+try:
+    # Database Constants
+    DB_HOSTNAME = conf.get('database','db_server',fallback='database')
+    DB_PORT = conf.get('database','db_port',fallback='5432')
+    DB_NAME = conf.get('database','db_name',fallback='poweripam')
+    DB_USER = conf.get('database','db_username',fallback='postgres')
+    DB_PASSWORD = conf.get('database','db_password',fallback='postgres')
 
-# Database Constants
-DB_HOSTNAME = conf.get('database','db_server',fallback='database')
-DB_PORT = conf.get('database','db_port',fallback='5432')
-DB_NAME = conf.get('database','db_name',fallback='poweripam')
-DB_USER = conf.get('database','db_username',fallback='postgres')
-DB_PASSWORD = conf.get('database','db_password',fallback='postgres')
-
-DB_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-logging.debug(f'{DB_URI=}')
+    DB_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
+    # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+    logging.debug(f'{DB_URI=}')
 
 
-# Logging Constants
-LOG_LEVEL = logging.getLevelNamesMapping()[conf.get('logging','log_level',fallback='info').upper()]
-LOG_FILE_LEVEL = logging.getLevelNamesMapping()[conf.get('logging','log_file_level',fallback='warning').upper()]
-logging.debug(f'{LOG_FILE_LEVEL=}')
-LOG_FILE = conf.get('logging','log_file',fallback='PowerIPAM.log')
-LOG_DIR = conf.get('logging','log_dir',fallback='./logs')
+    # Logging Constants
+    LOG_LEVEL = logging.getLevelNamesMapping()[conf.get('logging','log_level',fallback='info').upper()]
+    LOG_FILE_LEVEL = logging.getLevelNamesMapping()[conf.get('logging','log_file_level',fallback='warning').upper()]
+    logging.debug(f'{LOG_FILE_LEVEL=}')
+    LOG_FILE = conf.get('logging','log_file',fallback='PowerIPAM.log')
+    LOG_DIR = conf.get('logging','log_dir',fallback='./logs')
 
-LOG_LOCATION = f'{LOG_DIR}/{LOG_FILE}'
+    LOG_LOCATION = f'{LOG_DIR}/{LOG_FILE}'
+finally:
+    mod_logger.debug('Leaving module %s', current_frame_name)
 
--- main/app/protos/ipAddress_pb2.pyi
+++ main/app/protos/ipAddress_pb2.pyi
@@ -11,7 +11,7 @@ from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class IpAddress(_message.Message):
-    __slots__ = ("id", "ip_address", "owner", "description", "is_gateway", "hostname", "state_id", "last_seen", "last_modified", "date_created", "subnet_id", "mac_address")
+    __slots__ = ("id", "ip_address", "owner", "description", "is_gateway", "hostname", "state_id", "is_available", "last_seen", "last_modified", "date_created", "subnet_id", "mac_address")
     ID_FIELD_NUMBER: _ClassVar[int]
     IP_ADDRESS_FIELD_NUMBER: _ClassVar[int]
     OWNER_FIELD_NUMBER: _ClassVar[int]
@@ -19,6 +19,7 @@ class IpAddress(_message.Message):
     IS_GATEWAY_FIELD_NUMBER: _ClassVar[int]
     HOSTNAME_FIELD_NUMBER: _ClassVar[int]
     STATE_ID_FIELD_NUMBER: _ClassVar[int]
+    IS_AVAILABLE_FIELD_NUMBER: _ClassVar[int]
     LAST_SEEN_FIELD_NUMBER: _ClassVar[int]
     LAST_MODIFIED_FIELD_NUMBER: _ClassVar[int]
     DATE_CREATED_FIELD_NUMBER: _ClassVar[int]
@@ -31,12 +32,13 @@ class IpAddress(_message.Message):
     is_gateway: bool
     hostname: str
     state_id: _common_pb2.StateId
+    is_available: bool
     last_seen: _timestamp_pb2.Timestamp
     last_modified: _timestamp_pb2.Timestamp
     date_created: _timestamp_pb2.Timestamp
     subnet_id: str
     mac_address: str
-    def __init__(self, id: _Optional[str] = ..., ip_address: _Optional[str] = ..., owner: _Optional[str] = ..., description: _Optional[str] = ..., is_gateway: bool = ..., hostname: _Optional[str] = ..., state_id: _Optional[_Union[_common_pb2.StateId, str]] = ..., last_seen: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., last_modified: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., date_created: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., subnet_id: _Optional[str] = ..., mac_address: _Optional[str] = ...) -> None: ...
+    def __init__(self, id: _Optional[str] = ..., ip_address: _Optional[str] = ..., owner: _Optional[str] = ..., description: _Optional[str] = ..., is_gateway: bool = ..., hostname: _Optional[str] = ..., state_id: _Optional[_Union[_common_pb2.StateId, str]] = ..., is_available: bool = ..., last_seen: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., last_modified: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., date_created: _Optional[_Union[datetime.datetime, _timestamp_pb2.Timestamp, _Mapping]] = ..., subnet_id: _Optional[str] = ..., mac_address: _Optional[str] = ...) -> None: ...
 
 class IpAddressRequest(_message.Message):
     __slots__ = ("id", "name", "subnet_id")
